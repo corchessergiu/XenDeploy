3f37ce2982c1473b04507c78f92673de5c6d904a
corchessergiu <corchessergiu194@gmail.com>
2023-05-14 01:02:07 +0300

    update networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,8 +4,19 @@ require("@nomicfoundation/hardhat-toolbox");
 // project. It imports a Hardhat task definition, that can be used for
 // testing the frontend.
 require("./tasks/faucet");
+const { mnemonic } = require('./.secrets.json')
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+    solidity: "0.8.17",
+    networks: {
+        scrollAlpha: {
+            url: "https://alpha-rpc.scroll.io/12" || "",
+            chainId: 137,
+            gasPrice: "auto",
+            allowUnlimitedContractSize: true,
+            timeout: 1800000,
+            accounts: { mnemonic: mnemonic },
+        }
+    }
 };
