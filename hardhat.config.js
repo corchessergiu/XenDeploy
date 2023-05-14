b405011e2050f4d4019b7b5da9d1b5e4f1044d3b
corchessergiu <corchessergiu194@gmail.com>
2023-05-14 03:34:51 +0300

    update data

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle");
 
 // The next line is part of the sample project, you don't need it in your
 // project. It imports a Hardhat task definition, that can be used for
@@ -6,17 +7,42 @@ require("@nomicfoundation/hardhat-toolbox");
 require("./tasks/faucet");
 const { mnemonic } = require('./.secrets.json')
 
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 module.exports = {
-    solidity: "0.8.17",
+    solidity: {
+        compilers: [{
+            version: "0.8.17",
+            settings: {
+                optimizer: {
+                    enabled: true,
+                    runs: 1
+                }
+            }
+        }]
+    },
     networks: {
         scrollAlpha: {
-            url: "https://alpha-rpc.scroll.io/12" || "",
-            chainId: 137,
+            url: "https://alpha-rpc.scroll.io/l2" || "",
+            chainId: 534353,
             gasPrice: "auto",
             allowUnlimitedContractSize: true,
             timeout: 1800000,
             accounts: { mnemonic: mnemonic },
         }
+    },
+    etherscan: {
+        apiKey: {
+            scrollAlpha: '66C33X1T2D9UA4ZU6G94MD66AHIRBG54D2',
+        },
+        customChains: [{
+            network: 'scrollAlpha',
+            chainId: 534353,
+            urls: {
+                apiURL: 'https://blockscout.scroll.io/api',
+                browserURL: 'https://blockscout.scroll.io/',
+            },
+        }, ],
     }
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,58 +4,67 @@
 const path = require("path");
 
 async function main() {
-  // This is just a convenience check
-  if (network.name === "hardhat") {
-    console.warn(
-      "You are trying to deploy a contract to the Hardhat Network, which" +
-        "gets automatically created and destroyed every time. Use the Hardhat" +
-        " option '--network localhost'"
+    // This is just a convenience check
+    if (network.name === "hardhat") {
+        console.warn(
+            "You are trying to deploy a contract to the Hardhat Network, which" +
+            "gets automatically created and destroyed every time. Use the Hardhat" +
+            " option '--network localhost'"
+        );
+    }
+
+    // ethers is available in the global scope
+    const [deployer] = await ethers.getSigners();
+    console.log(
+        "Deploying the contracts with the account:",
+        await deployer.getAddress()
     );
-  }
 
-  // ethers is available in the global scope
-  const [deployer] = await ethers.getSigners();
-  console.log(
-    "Deploying the contracts with the account:",
-    await deployer.getAddress()
-  );
+    console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  console.log("Account balance:", (await deployer.getBalance()).toString());
+    const lib = await ethers.getContractFactory("Math");
+    const library = await lib.deploy();
 
-  const Token = await ethers.getContractFactory("Token");
-  const token = await Token.deploy();
-  await token.deployed();
+    const xenContract = await ethers.getContractFactory("XENCrypto", {
+        libraries: {
+            Math: library.address
+        }
+    });
 
-  console.log("Token address:", token.address);
+    XENContract = await xenContract.deploy();
+    await XENContract.deployed();
 
-  // We also save the contract's artifacts and address in the frontend directory
-  saveFrontendFiles(token);
+
+    console.log("Token address:", XENContract.address);
+
+    // We also save the contract's artifacts and address in the frontend directory
+    saveFrontendFiles(XENContract);
 }
 
 function saveFrontendFiles(token) {
-  const fs = require("fs");
-  const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
+    const fs = require("fs");
+    const contractsDir = path.join(__dirname, "..", "frontend", "src", "contracts");
 
-  if (!fs.existsSync(contractsDir)) {
-    fs.mkdirSync(contractsDir);
-  }
+    if (!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir);
+    }
 
-  fs.writeFileSync(
-    path.join(contractsDir, "contract-address.json"),
-    JSON.stringify({ Token: token.address }, undefined, 2)
-  );
+    fs.writeFileSync(
+        path.join(contractsDir, "contract-address.json"),
+        JSON.stringify({ Token: XENContract.address }, undefined, 2)
+    );
 
-  const TokenArtifact = artifacts.readArtifactSync("Token");
+    const XENContractArtifact = artifacts.readArtifactSync("XENContract");
 
-  fs.writeFileSync(
-    path.join(contractsDir, "Token.json"),
-    JSON.stringify(TokenArtifact, null, 2)
-  );
+    fs.writeFileSync(
+        path.join(contractsDir, "XENContract.json"),
+        JSON.stringify(XENContractArtifact, null, 2)
+    );
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
